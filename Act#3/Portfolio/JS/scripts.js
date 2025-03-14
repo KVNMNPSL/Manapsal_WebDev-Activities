f95a7b0ac7bb9cdea8877529fa5d3430544966ef
Skye1104 <kevinmanapsal11@gmail.com>
2025-03-14 19:08:18 +0800

    portfolio done but not final
    03/14/25

---
+++ Act#3/Portfolio/JS/scripts.js
@@ -0,0 +1,8 @@
+// Leaflet Map
+
+var map = L.map('map').setView([14.599512, 120.984222], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
